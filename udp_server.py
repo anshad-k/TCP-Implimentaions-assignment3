666b36be9af866c4dcdb306fdd1d1e77fe99d32e
anshadkonnola <ae20b035@smail.iitm.ac.in>
2024-05-05 15:04:41 +0530

    add: stop and wait

--- udp_server.py
+++ udp_server.py
@@ -1,34 +1,10 @@
 import socket
+import sys
 import time
-
-class Timer(object):
-    TIMER_STOP = -1
-
-    def __init__(self, duration):
-        self._start_time = self.TIMER_STOP
-        self._duration = duration
-
-    # Starts the timer
-    def start(self):
-        if self._start_time == self.TIMER_STOP:
-            self._start_time = time.time()
-
-    # Stops the timer
-    def stop(self):
-        if self._start_time != self.TIMER_STOP:
-            self._start_time = self.TIMER_STOP
-
-    # Determines whether the timer is runnning
-    def running(self):
-        return self._start_time != self.TIMER_STOP
-
-    # Determines whether the timer timed out
-    def timeout(self):
-        if not self.running():
-            return False
-        else:
-            return time.time() - self._start_time >= self._duration
-        
+import _thread
+import udt
+from timer import Timer
+import packet
 
 class UDPServer:
     SW = 0
@@ -44,7 +20,10 @@ class UDPServer:
         self.packet_size = packet_size
         self.window_size = window_size
         self.RTO = RTO
+        self.timer = Timer(RTO)
         self.__sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.mutex = _thread.allocate_lock()
+        self.base = 0
     
     def run(self, tcp_algorithm):
         self.__sock.bind(("", self.port))
@@ -52,20 +31,69 @@ class UDPServer:
         if file is None:
             print("File not found")
             return
-        if tcp_algorithm == UDPServer.SW:
-            self.__stop_and_wait()
-        elif tcp_algorithm == UDPServer.GBN:
-            self.__go_back_n()
-        elif tcp_algorithm == UDPServer.SR:
-            self.__selective_repeat()
-        else:
-            print("Invalid TCP algorithm")
-    def __stop_and_wait(self):
+        while True:
+            message, address = self.__sock.recvfrom(32)
+            if message != b"SYN":
+                continue
+            if tcp_algorithm == UDPServer.SW:
+                self.__stop_and_wait(address)
+            elif tcp_algorithm == UDPServer.GBN:
+                self.__go_back_n(address)
+            elif tcp_algorithm == UDPServer.SR:
+                self.__selective_repeat(address)
+            else:
+                print("Invalid TCP algorithm")
+    def __stop_and_wait(self, client_address):
+        packets = []
+        seq_num = 0
+        while True:
+            data = self.file.read(self.packet_size)
+            if not data:
+                break
+            packets.append(packet.make(seq_num, data))
+            seq_num += 1
+        
+        num_packets = len(packets)
+        self.current_packet = 0
+
+        _thread.start_new_thread(self.__SW_receive, (client_address))
+
+        while self.current_packet < num_packets:
+            self.mutex.acquire()
+            udt.send(packets[self.current_packet], self.__sock, client_address)
+            print("Sent packet", self.current_packet)
+            self.timer.stop()
+            self.timer.start()
+            sent_packet = self.current_packet
+            self.mutex.release()
+
+            while not self.timer.timeout():
+                self.mutex.acquire()
+                if self.current_packet > sent_packet:
+                    break
+                self.mutex.release()
+            self.mutex.release()
+        udt.send(packet.make_empty(), self.__sock, client_address)
+        print("File sent")
+    
+    def __SW_receive(self, client_address):
+        while True:
+            pkt, address = udt.recv(self.__sock)
+            ack, _ = packet.extract(pkt)
+            if address != client_address:
+                continue
+            if ack == self.current_packet:
+                self.mutex.acquire()
+                print("Received ACK", ack)
+                self.current_packet += 1
+                self.mutex.release()
+
+
+    def __go_back_n(self, client_address):
         pass
-    def __go_back_n(self):
+    def __selective_repeat(self, client_address):
         pass
-    def __selective_repeat(self):
-        pass
+
     def __del__(self):
         if self.__sock is not None:
           self.__sock.close()
